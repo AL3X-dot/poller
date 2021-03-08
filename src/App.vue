f6fa239e2d8ed42c9bb88730b69fcf05da2560fa
AL3X-dot <amitdave1998@gmail.com>
2021-03-08 17:37:09 +0530

    Day1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="nav-wrapper grey lighten-3 z-depth-2 valign-wrapper">
+    <div class="brand-logo black-text center ">
+      <span><i class="material-icons">done_all</i>Pollitout</span>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="row">
+      <div class="col l8 offset-l2 m8 offset-m2 s12">
+        <div class="card">
+          <div class="card-image">
+            <img src="./assets/anonymous.jpg" alt="anonymous" class="responsive-img">
+            <span class="card-title left">You are Anonymous</span>
+          </div>
+          <div class="card-action">
+            <div class="row center">
+              <a class="blue-text text-darken-1 activator" @click="create">Create A Poll</a>
+            </div>
+            <div class="row center">
+              <a class="blue-text text-darken-1 activator" @click='join'>Join A Poll</a>
+            </div>
+          </div>
+          <div :class='c_c_class' :style="hide" id="create">
+            <span class="card-title grey-text text-darken-4">Create<i class="material-icons right">close</i></span>
+            <div class="row center input-field valign-wrapper">
+              <div class="col s10 input-field">
+                <label for="">Enter new options</label>
+                <input type="text" v-model="option">
+              </div>
+              <div class="col s2">
+                <i class="material-icons" style="cursor:pointer" @click='addOption'>add</i>
+              </div>
+            </div>
+          </div>
+          <div :class="j_c_class" :style="hide" id="join">
+            <span class="card-title grey-text text-darken-4">Join<i class="material-icons right">close</i></span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data(){
+    return{
+      c_c_class:'',
+      show:'display:',
+      j_c_class:'',
+      hide:"display:none",
+      option:'',
+      options:[]
+    }
+  },
+  created(){
+  },
+  methods:{
+    create(){
+      this.j_c_class=''
+      this.c_c_class='card-reveal'
+      this.c_c_display=''
+    },
+    join(){
+      this.c_c_class=''
+      this.j_c_class='card-reveal'
+      this.j_c_display=''
+    },
+    addOption(){
+      console.log(this.option);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .material-icons{
+    margin-top:5px
+  }
+  .myTitle{
+    font-weight: 400;
+  }
 </style>
